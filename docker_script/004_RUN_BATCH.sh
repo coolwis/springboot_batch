79d7d09bd5d4ba8fa25bd49f1b622a6bf4a89165
coolwis <wisdombrave@gmail.com>
2022-02-14 17:40:44 +0900

    s

--- docker_script/004_RUN_BATCH.sh
+++ docker_script/004_RUN_BATCH.sh
@@ -1,7 +1,7 @@
 docker run -itd --name docker_batch_app \
     --network docker_batch_network \
     -e SPRING_PROFILES_ACTIVE=docker \
-    -v /app/r100/INFILES:/INFILES \
+    -v /app/springboot_batch/INFILES:/INFILES \
     docker_batch_app_image
 
 
